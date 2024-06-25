428ecd075a1efd7642d146a65bc82e9229530d00
Evelline <elinyine@gmail.com>
2024-06-25 09:10:57 +0700

    install laravel and api route

---
+++ routes/api.php
@@ -0,0 +1,16 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\AuthController;
+
+Route::prefix('user')->group(function () {
+    Route::get('/users', function () {
+        return $request->user();
+    });
+    Route::post('/register', [AuthController::class, 'register']);
+    Route::get('/login', [AuthController::class, 'login'])->name('login');
+    Route::post('/login', [AuthController::class, 'login']);
+    Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:api');
+});
+
